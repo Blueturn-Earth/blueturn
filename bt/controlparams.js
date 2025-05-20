90d3ab78553ec916ef02aa2d13522ccc63e9e730
Michael Boccara <michaelboccara@users.noreply.github.com>
2025-05-20 09:45:21 +0000

    try to pass messages

--- bt/controlparams.js
+++ bt/controlparams.js
@@ -9,9 +9,20 @@ controlMap.set('date', (v) => {gDate = v;});
 
 const urlParams = new URLSearchParams(window.location.search);
 
-controlMap.forEach((cb, param, cmap) => {
+controlMap.forEach((cb, param) => {
     const paramValue = urlParams.get(param);
     if (paramValue === null)
         return;
     cb(paramValue);
+});
+
+window.addEventListener("message", (event) => {
+    //if (event.origin !== "https://app.blueturn.earth") return; // security check
+    console.log("Received message: ", event.data);
+    controlMap.forEach((cb, param) => {
+        if (event.data.type === param) {
+            console.log("Handling message: ", event.data);
+            cb(event.data.value);
+        }
+    });
 });
