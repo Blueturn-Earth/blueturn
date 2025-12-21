d35a9e0689081fb7e97699c5ba33865d22caafd7
LAPTOP-MR4O10NF\micboq <micboq@gmail.com>
2025-12-22 00:22:23 +0200

    Add support for picking image file

--- bt/camera.js
+++ bt/camera.js
@@ -1,5 +1,6 @@
 import GoogleDriveProvider from './gdrive_provider.js';
 import {saveMetadata} from './firebase_save.js';
+import {processEXIF} from './exif.js'
 
 const modal = document.getElementById('photoModal');
 const modalImage = document.getElementById('modalImage');
@@ -11,6 +12,8 @@ const loading = document.getElementById("loadingOverlay");
 
 let latestOrientation = { alpha: 0, beta: 0, gamma: 0 };
 let latestGPS = null;
+let latestTakenTime;
+let latestImageURL;
 
 // Ask for DeviceOrientation permission on iOS
 function enableOrientation() {
@@ -34,11 +37,26 @@ function handleOrientation(e) {
 console.log("Enabling device orientation");
 enableOrientation();
 
+const cameraButton = document.getElementById("cameraButton");
+const cameraInput = document.getElementById("cameraInput");
+cameraInput.style.display = "none";
+
 console.log("Add camera button click handler");
 // Capture photo and metadata
-document.getElementById("cameraButton").addEventListener("click", async () => {
+cameraButton.addEventListener("click", async () => {
   // Trigger camera
-  document.getElementById("cameraInput").click();
+  cameraInput.click();
+});
+
+const imageFileButton = document.getElementById("imageFileButton");
+const imageFileInput = document.getElementById("imageInput");
+imageFileInput.style.display = "none";
+
+console.log("Add image picker button click handler");
+// Capture photo and metadata
+imageFileButton.addEventListener("click", async () => {
+  // Trigger image file picker
+  imageFileInput.click();
 });
 
 
@@ -82,12 +100,36 @@ function analyzeSky(img) {
 
 }
 
-let preparedImageDataURL = null;
+function updateModal()
+{
+  // Show modal
+  modalImage.src = latestImageURL;
+  modalTimestamp.textContent = latestTakenTime;
+  modalGPS.textContent = latestGPS ? `GPS: ${latestGPS.lat.toFixed(6)}, ${latestGPS.lon.toFixed(6)}` : "GPS: unavailable";
+
+  // Sky coverage
+  const img = new Image();
+  img.onload = () => {
+    const isSky = analyzeSky(img);
+    console.log("Sky photo analysis:", isSky);
+  };
+  console.log("Loading image");
+  img.src = latestImageURL;
+
+  console.log("Show modal");
+  // All ready → Hide spinner + show modal
+  loading.style.display = "none";
+  modal.style.display = 'flex';
+  labelEl.style.display = "none";
+  barEl.style.width = "0%";
+}
 
 console.log("Add camera input change handler");
 // When the user takes a picture
 document.getElementById("cameraInput").addEventListener("change", async (event) => {
-  preparedImageDataURL = null;
+  latestImageURL = undefined;
+  latestTakenTime = undefined;
+  latestGPS = undefined;
 
   const file = event.target.files[0];
   if (!file) return;
@@ -104,116 +146,56 @@ document.getElementById("cameraInput").addEventListener("change", async (event) 
       latestGPS = { lat: pos.coords.latitude, lon: pos.coords.longitude, alt: pos.coords.altitude };
     } catch (err) {
       console.warn("GPS not available:", err.message);
+      return;
     }
   }
 
-  // Attempt to get GPS from EXIF (optional, only if stored)
-  if (!latestGPS) {
-    console.log("Trying to get GPS from EXIF");
-    EXIF.getData(file, function() {
-        const tags = EXIF.getAllTags(this);
-        if (tags.GPSLatitude && tags.GPSLongitude) {
-            const lat = tags.GPSLatitude[0] + tags.GPSLatitude[1]/60 + tags.GPSLatitude[2]/3600;
-            const lon = tags.GPSLongitude[0] + tags.GPSLongitude[1]/60 + tags.GPSLongitude[2]/3600;
-            latestGPS = { lat, lon };
-            console.log("Got GPS from EXIF:", latestGPS);
-        }
-        else {
-            console.log("No GPS in EXIF data");
-        }
-    });
-  }
+  // Get createdAt
+  const createdAt = new Date().toISOString();
+  latestTakenTime = createdAt;
+  console.log("Using current time as taken time:", latestTakenTime);
 
-  // Get timestamp
-  const timestamp = new Date().toISOString();
+  latestImageURL = URL.createObjectURL(file);
 
- // Show modal
-  modalImage.src = URL.createObjectURL(file);
-  modalTimestamp.textContent = timestamp;
-  modalGPS.textContent = latestGPS ? `GPS: ${latestGPS.lat.toFixed(6)}, ${latestGPS.lon.toFixed(6)}` : "GPS: unavailable";
-
-  // Sky coverage
-  const img = new Image();
-  img.onload = () => {
-    const isSky = analyzeSky(img);
-    console.log("Sky photo analysis:", isSky);
-  };
-  console.log("Loading image");
-  img.src = URL.createObjectURL(file);
-
-  console.log("Adding EXIF data to image…");
-  preparedImageDataURL = await addExif(
-    file,
-    latestGPS
-  );
-
-  console.log("Show modal");
-  // All ready → Hide spinner + show modal
-  loading.style.display = "none";
-  modal.style.display = 'flex';
-  labelEl.style.display = "none";
-  barEl.style.width = "0%";
+  // Show modal
+  updateModal();
 });
 
+console.log("Add image file input change handler");
+// When the user takes a picture
+document.getElementById("imageInput").addEventListener("change", async (event) => {
+  latestImageURL = undefined;
+  latestTakenTime = undefined;
+  latestGPS = undefined;
+
+  const imgFile = event.target.files[0];
+  if (!imgFile) return;
+
+  // Show loading spinner immediately after accepting the photo
+  loading.style.display = "flex";
+  try {
+    const result = await processEXIF(imgFile);
+    latestTakenTime = result.takenTime;
+    latestGPS = result.gps;
+  }
+  catch(e) {
+    const errorMsg = "Cannot use selected image";
+    alert(errorMsg);
+    console.error(errorMsg);
+    loading.style.display = "none";
+    modal.style.display = 'none';
+    labelEl.style.display = "none";
+    barEl.style.width = "0%";
+    return;
+  }
+
+  latestImageURL = URL.createObjectURL(imgFile);
+
+  updateModal();
+});
 
 // Close modal when clicking outside or on close button
 closeModal.addEventListener('click', () => modal.style.display='none');
-
-function toRational(number) {
-  const denom = 1000000;
-  return [Math.round(number * denom), denom];
-}
-
-function gpsToExif(coord) {
-  const abs = Math.abs(coord);
-  const deg = Math.floor(abs);
-  const minFloat = (abs - deg) * 60;
-  const min = Math.floor(minFloat);
-  const sec = (minFloat - min) * 60;
-  return [[deg,1], [min,1], toRational(sec)];
-}
-
-async function addExif(file, gps) {
-  const dataURL = await fileToDataURL(file);
-  const exifObj = piexif.load(dataURL);
-
-  // Timestamp
-  const now = new Date();
-  const exifDate =
-    now.getFullYear() + ":" +
-    String(now.getMonth()+1).padStart(2,"0") + ":" +
-    String(now.getDate()).padStart(2,"0") + " " +
-    String(now.getHours()).padStart(2,"0") + ":" +
-    String(now.getMinutes()).padStart(2,"0") + ":" +
-    String(now.getSeconds()).padStart(2,"0");
-
-  exifObj["0th"][piexif.ImageIFD.DateTime] = exifDate;
-  exifObj["Exif"][piexif.ExifIFD.DateTimeOriginal] = exifDate;
-
-  // GPS
-  if (gps) {
-    exifObj["GPS"][piexif.GPSIFD.GPSLatitudeRef]  = gps.lat >= 0 ? "N" : "S";
-    exifObj["GPS"][piexif.GPSIFD.GPSLongitudeRef] = gps.lon >= 0 ? "E" : "W";
-    exifObj["GPS"][piexif.GPSIFD.GPSLatitude]  = gpsToExif(gps.lat);
-    exifObj["GPS"][piexif.GPSIFD.GPSLongitude] = gpsToExif(gps.lon);
-
-    if (gps.alt != null) {
-      exifObj["GPS"][piexif.GPSIFD.GPSAltitude] = toRational(Math.abs(gps.alt));
-      exifObj["GPS"][piexif.GPSIFD.GPSAltitudeRef] = gps.alt < 0 ? 1 : 0;
-    }
-  }
-
-  const exifBytes = piexif.dump(exifObj);
-  return piexif.insert(exifBytes, dataURL);
-}
-
-function fileToDataURL(file) {
-  return new Promise(res => {
-    const r = new FileReader();
-    r.onload = () => res(r.result);
-    r.readAsDataURL(file);
-  });
-}
 
 const progressEl = document.getElementById("uploadProgress");
 const barEl = progressEl.querySelector(".bar");
@@ -269,8 +251,8 @@ async function saveImage(dataURL) {
 
 document.getElementById("saveImageBtn").addEventListener("click", async (e) => {
   e.stopPropagation(); // empêche la popup de se fermer
-  if (!preparedImageDataURL) return;
+  if (!latestImageURL) return;
 
-  await saveImage(preparedImageDataURL);
+  await saveImage(latestImageURL);
 });
 
---
+++ bt/exif.js
@@ -0,0 +1,46 @@
+import tzLookup from 'https://esm.sh/@photostructure/tz-lookup@latest';
+import * as exifr from 'https://cdn.jsdelivr.net/npm/exifr/dist/full.esm.js';
+
+export async function processEXIF(imgFile)
+{
+    console.log("Trying to get GPS from EXIF");
+    // Get createdAt
+    const createdAt = new Date().toISOString();
+    let takenTime = undefined;
+    let gps = undefined;
+
+    const tags = await exifr.parse(imgFile, { 
+            tiff: true, 
+            ifd0: true, 
+            exif: true, 
+            gps: true,
+            pick: ['DateTimeOriginal','CreateDate','ModifyDate','GPSLatitude','GPSLongitude','GPSLatitudeRef','GPSLongitudeRef']
+        });
+  
+    if (tags.GPSLatitude && tags.GPSLongitude) {
+        const lat = tags.GPSLatitude[0] + tags.GPSLatitude[1]/60 + tags.GPSLatitude[2]/3600;
+        const lon = tags.GPSLongitude[0] + tags.GPSLongitude[1]/60 + tags.GPSLongitude[2]/3600;
+        gps = { lat, lon };
+        console.log("Got GPS from EXIF:", gps);
+    }
+    else {
+        const errorMsg = "No GPS in image file";
+        alert(errorMsg);
+        throw new Error(errorMsg);
+    }
+
+    const dateTaken = 
+        tags.DateTimeOriginal || 
+        tags.CreateDate || 
+        tags.ModifyDate;
+    if (!dateTaken) {
+        const errorMsg = "No Timestamp in image file";
+        alert(errorMsg);
+        throw new Error(errorMsg);
+    }
+    console.log('EXIF date:', dateTaken);
+    return {
+        takenTime: dateTaken,
+        gps: gps
+    };
+}
