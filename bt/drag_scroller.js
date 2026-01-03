78af34f47d98c16283b5e4ef90928bd0bd502251
LAPTOP-MR4O10NF\micboq <micboq@gmail.com>
2026-01-03 22:22:14 +0200

    fix some off-by-one errors

--- bt/drag_scroller.js
+++ bt/drag_scroller.js
@@ -171,7 +171,7 @@ export default class DragScroller
     if (index == this.selectedItemIndex)
     {
       if (this.onSelectedItemClickCb)
-        this.onSelectedItemClickCb(this.itemsGroup.children[index + 1], index); // skip start spacer+template
+        this.onSelectedItemClickCb(this.itemsGroup.children[index + 2], index); // skip start spacer+template
       return;
     }
 
@@ -181,7 +181,7 @@ export default class DragScroller
     // skip start spacer+template
     const childIndex = index + 2; 
     // forbid spacers
-    if (childIndex < 1 || childIndex >= children.length - 1)
+    if (childIndex < 2 || childIndex >= children.length - 1)
     {
       console.warn("scrollToIndex: index out of bounds: ", index);
       return;
